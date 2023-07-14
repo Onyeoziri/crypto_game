9ff8899b2349cef03a76341135d0e8a43d2a334b
jchen753951 <124069788+jchen753951@users.noreply.github.com>
2023-07-14 16:30:32 -0400

    Add files via upload

---
+++ Contract/contracts/nft.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract NFTTransferContract is IERC721Receiver {
+    using Address for address;
+
+    address private _owner;
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    function transferNFT(address nftContract, uint256 tokenId, address recipient) external {
+        require(msg.sender == _owner, "Only the contract owner can call this function");
+
+        IERC721(nftContract).safeTransferFrom(address(this), recipient, tokenId);
+    }
+
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data) external view override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}

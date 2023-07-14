0cf63ee95464ef5e031632a66fb81497062c3940
jchen753951 <124069788+jchen753951@users.noreply.github.com>
2023-07-14 16:56:58 -0400

    Add files via upload

--- Contract/contracts/nft.sol
+++ Contract/contracts/nft.sol
@@ -6,6 +6,12 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
 contract NFTTransferContract is IERC721Receiver {
     using Address for address;
 
@@ -19,6 +25,12 @@ contract NFTTransferContract is IERC721Receiver {
         require(msg.sender == _owner, "Only the contract owner can call this function");
 
         IERC721(nftContract).safeTransferFrom(address(this), recipient, tokenId);
+    }
+
+    function mintNFT(address nftContract, address recipient, uint256 tokenId) external {
+        require(msg.sender == _owner, "Only the contract owner can call this function");
+
+        IERC721(nftContract).safeTransferFrom(msg.sender, recipient, tokenId);
     }
 
     function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data) external view override returns (bytes4) {

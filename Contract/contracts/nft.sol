6e6f6e574decf7ab7a6706a9180157fe6d7149d0
jchen753951 <124069788+jchen753951@users.noreply.github.com>
2023-07-17 12:52:17 -0400

    Add files via upload

--- Contract/contracts/nft.sol
+++ Contract/contracts/nft.sol
@@ -1,11 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-
+//Jin Chen
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";

155fe436c98d8f777c589b86704e613a8083103c
vrauli5615 <tori.raulin@gmail.com>
2023-07-18 11:28:52 -0400

    Update NewNFT.sol

--- NewNFT.sol
+++ NewNFT.sol
@@ -16,6 +16,16 @@ contract NewNFT is ERC721URIStorage, Ownable {
 		_mint(_target, _tokenID);
 		_setTokenURI(_tokenID, _URI);
 	}
+     function transferNFT(address nftContract, uint256 tokenId, address recipient) external {
+        require(msg.sender == _owner, "Only the contract owner can call this function");
+
+        IERC721(nftContract).safeTransferFrom(address(this), recipient, tokenId);
+    }
+
+    // function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data) external view override returns (bytes4) {
+    //     return this.onERC721Received.selector;
+    // }
+
 }
 
 
